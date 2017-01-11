4e386986010f14dc60ac337713854410e5ba6b42
Abbey Ola <abbey.ola@massive.co>
2017-01-11 20:49:17 +0100

    first commit

---
+++ mobile/src/main/java/com/example/android/mcv/ui/HelpActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.mcv.ui;
+
+import android.os.Bundle;
+
+import com.example.android.mcv.R;
+
+/**
+ * Created by abbey.ola on 10/01/2017.
+ */
+
+public class HelpActivity extends BaseActivity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+		initializeToolbar();
+
+	}
+}
